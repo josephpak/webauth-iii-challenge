c36613aace6a54d25bdeeada96fb5897276e32ea
joepak <josephpak2014@gmail.com>
2019-04-03 16:06:09 -0400

    File structure for React client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import { GlobalStyle } from "./components/styles/Global";
+
+// Display Components
+import UsersList from "./components/UsersList";
+
+// Auth Components
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+// Miscellaneous Components
+import NotFound from "./components/NotFound";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <GlobalStyle />
+        This is the application
+        <Switch>
+					<Route exact path="/users" component={UsersList} />
+
+					<Route path="/login" exact component={Login} />
+					<Route path="/register" exact component={Register} />
+					<Route component={NotFound} />
+				</Switch>
+      </>
     );
   }
 }
---
+++ client/src/components/UsersList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class UsersList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
