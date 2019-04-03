f03415c5503c9fc59095d9a6345d5e09ff6b4f27
joepak <josephpak2014@gmail.com>
2019-04-03 14:52:19 -0400

    Data retrieval helpers

---
+++ users/users-model.js
@@ -0,0 +1,21 @@
+const db = require('../data/dbConfig.js')
+
+module.exports = {
+    add,
+    find,
+    findBy
+}
+
+function find() {
+    return db('users')
+}
+
+function findBy(filter) {
+    return db('users')
+        .where(filter)
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user)
+    return findBy({id})
+}
