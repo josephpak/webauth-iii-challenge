d63b1ae19acb885f0b9aa2253940b9db9c101591
joepak <josephpak2014@gmail.com>
2019-04-03 15:31:19 -0400

    JWT support and restriction middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const restrict = require('../middleware/restrict.js')
 
 const authRouter = require('../auth/auth-router.js')
 const usersRouter = require('../users/users-router.js')
@@ -12,6 +13,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter)
-server.use('/api/users', usersRouter)
+server.use('/api/users', restrict, usersRouter)
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('./secrets.js')
 const router = express.Router();
 
 const Users = require('../users/users-model.js')
@@ -25,8 +27,11 @@ router.post('/login', async(req,res) => {
         } = req.body
         const user = await Users.findBy({username})
         if (user && bcrypt.compareSync(password, user.password)) {
+            const token = createToken(user)
+
             res.status(200).json({
-                message: `Welcome ${user.username}`
+                message: `Welcome ${user.username}`,
+                token
             })
         } else {
             res.status(401).json({
@@ -36,8 +41,26 @@ router.post('/login', async(req,res) => {
     } catch (error) {
         res.status(500).json(error)
     }
-
-
 })
+
+function createToken(user) {
+    const {
+        id,
+        username,
+        department
+    } = user
+
+    const payload = {
+        subject: id,
+        username,
+        department
+    }
+    const secret = secrets.jwtSecret
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
