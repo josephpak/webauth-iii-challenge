4cc9c61db6d09742f37d8bd4b6b93f3f2f1d724c
joepak <josephpak2014@gmail.com>
2019-04-03 16:51:46 -0400

    Not Found page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import dennis from './assets/magic-word.gif'
 
 export default function NotFound() {
   return (
     <div>
-      
+      <img src={dennis}/>
     </div>
   )
 }
